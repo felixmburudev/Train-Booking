d5df9b45369d158288c0f5ca06fed1a13f890e18
Felix Mburu <felixmburumwangi@gmail.com>
2024-02-18 21:51:10 +0300

    create a store to store train's live seats

---
+++ frontend/src/data/Api.jsx
@@ -0,0 +1,38 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+// Create context
+const TrainStateContext = createContext();
+
+export const TrainStateProvider = ({ children, trainName }) => {
+  const [trainState, setTrainState] = useState({ trainName: '', remainingSeats: 0 });
+
+ 
+  const updateRemainingSeats = (newTrainState) => {
+    setTrainState(newTrainState);
+  };
+
+  useEffect(() => {
+    const socket = new WebSocket('http://localhost:3000');
+
+    socket.onmessage = (event) => {
+      const newTrainState = JSON.parse(event.data);
+      updateRemainingSeats(newTrainState);
+    };
+
+    socket.send(JSON.stringify({ trainName }));
+    return () => {
+      socket.close();
+    };
+  }, [trainName]); 
+
+  return (
+    <TrainStateContext.Provider value={{ trainState, updateRemainingSeats }}>
+      {children}
+    </TrainStateContext.Provider>
+  );
+};
+
+
+export const useTrainState = () => {
+  return React.useContext(TrainStateContext);
+};
